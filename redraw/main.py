29058316d6b49eed04aa70f84f1e9b5d21bf8eb2
Nanored4498 <nanored.mc@gmail.com>
2019-06-28 19:29:30 +0200

    beggining of a new method

--- redraw/main.py
+++ redraw/main.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import pylab as pl
 import numpy as np
+import sympy
 
 class Line:
 	def __init__(self, x1, y1, x2, y2, col=(0, 0, 0)):
@@ -131,10 +132,10 @@ def main():
 
 	################################################
 	# Importance of the vanish points of depth lines
-	center_coeff = 5
+	center_coeff = 6
 	# Importance of the horizontal lines on the ground
 	# to readjust the horizon lines
-	horizon_coeff = 2
+	horizon_coeff = 1.7
 	################################################
 	
 	# Computing the mean of horizontal lines coefficient
@@ -145,6 +146,8 @@ def main():
 
 	# Recompute vanishing points
 	gs = sorted(dots.keys(), key=lambda g: dots[g][0])
+	print(gs)
+	print(dots[gs[0]][0], dots[gs[1]][0], dots[gs[2]][0])
 	dx = 0.5 * (dots[gs[2]][0] - dots[gs[0]][0])
 	a = (dots[gs[0]][0] + center_coeff * dots[gs[1]][0] + dots[gs[2]][0]) / (2 + center_coeff) - dx
 	dy = 0.5 * (dots[gs[2]][1] - dots[gs[0]][1])
@@ -153,7 +156,9 @@ def main():
 	dots2 = {gs[i] : (a+i*dx, b+i*dy) for i in range(3)}
 	horizon2 = Line(a-3*dx, b-3*dy, a+3*dx, b+3*dy, horizon._col)
 	horizon2.plot()
-	bottom = horizon2.translate(0, H - 0.5 * (horizon2.y(im_x) + horizon2.y(im_x+im_w)))
+	bottom = horizon2.translate(0, im_y+im_h - 0.5 * (horizon2.y(im_x) + horizon2.y(im_x+im_w)))
+
+	ds = [None]*3
 
 	# Recompute parallel lines
 	datas = {}
@@ -173,6 +178,44 @@ def main():
 		dx += (p1[0] - p0[0]) / (nls-1) * (1 if g != gs[1] else center_coeff) / (2 + center_coeff)
 		dy += (p1[1] - p0[1]) / (nls-1) * (1 if g != gs[1] else center_coeff) / (2 + center_coeff)
 		datas[g] = x, y, a, b, col, nls
+		i = gs.index(g)
+		dx2 = (p1[0] - p0[0]) / (nls-1)
+		dy2 = (p1[1] - p0[1]) / (nls-1)
+		ds[i] = (dx2**2 + dy2**2)**0.5
+
+	xs = [dots[gs[i]][0] for i in range(3)]
+	ys = [dots[gs[i]][1] for i in range(3)]
+	cxs = [(xs[1]-xs[0])**(-2), center_coeff * 4*(xs[2]-xs[0])**(-2), (xs[2]-xs[1])**(-2)]
+	cys = [(ys[1]-ys[0])**(-2), center_coeff * 4*(ys[2]-ys[0])**(-2), (ys[2]-ys[1])**(-2)]
+	cds = (max(ds) - min(ds)) ** (-2)
+
+	V = sympy.symbols('x0 x2 y0 y2 d0 d2')
+	X0, X2, Y0, Y2, D0, D2 = V
+	X1 = (X0*D2 + X2*D0) / (D0 + D2)
+	Y1 = (Y0*D2 + Y2*D0) / (D0 + D2)
+	D1 = 2 * D0 * D2 / (D0 + D2)
+	X, Y, D = [X0, X1, X2], [Y0, Y1, Y2], [D0, D1, D2]
+	F = 0
+	for i in range(3):
+		F += cxs[i]*(X[i]-xs[i])**2 + cys[i]*(Y[i]-ys[i])**2 + cds*(D[i]-ds[i])**2
+	G = [F.diff(v) for v in V]
+	H = [[g.diff(v) for v in V] for g in G]
+
+	def newton(v2):
+		s = [(V[i], v2[i]) for i in range(6)]
+		fv = float(F.subs(s))
+		gv = np.array([float(g.subs(s)) for g in G])
+		print(fv, max(abs(gv)))
+		hv = np.array([[float(hi.subs(s)) for hi in h] for h in H])
+		hvi = np.linalg.inv(hv)
+		return v2 - hvi.dot(gv)
+	
+	v2 = np.array([xs[0], xs[2], ys[0], ys[2], ds[0], ds[2]])
+	for i in range(4):
+		v2 = newton(v2)
+	s = [(V[i], v2[i]) for i in range(6)]
+	xs, ys, ds = [float(x.subs(s)) for x in X], [float(y.subs(s)) for y in Y], [float(d.subs(s)) for d in D]
+
 	d0c, d0s = 0, 0
 	for g in datas:
 		a = datas[g][2]
@@ -189,9 +232,19 @@ def main():
 		elif 2*(d0 - da) > dx: da += dx
 		a += d0 - da
 		b += (d0 - da) * dy / dx
+		a += a - x
+		b += b - y
 		groups2[g] = []
+		xls = [l.x(im_bot) for l in groups[g]]
+		x0, x1 = min(xls), max(xls)
+		l0, l1 = Line(x, y, x0, im_bot), Line(x, y, x1, im_bot)
+		p0, p1 = bottom.inter(l0), bottom.inter(l1)
+		nls = len(xls)
+		dx2 = (p1[0] - p0[0]) / (nls-1)
+		dy2 = (p1[1] - p0[1]) / (nls-1)
+		print(g, (dx2**2 + dy2**2)**0.5)
 		for i in range(nls):
-			l = Line(x, y, a + i * dx, b + i * dy, col)
+			l = Line(x, y, a + 2 * i * dx2, b + 2 * i * dy2, col)
 			groups2[g].append(l)
 			l.plot()
 
